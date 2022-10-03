3d1a9a488dad1c6425b61a01449808a41175112c
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-10-02 19:10:49 -0700

    Initial Commit.

---
+++ northwind-queries-Imani-Jones.sql
@@ -0,0 +1,55 @@
+-- What are the categories of products in the database?
+SELECT category
+FROM products p 
+ 
+
+-- What products are made by Dell?
+SELECT *
+FROM products p 
+WHERE product_name
+LIKE '%Dell%'
+
+
+-- List all the orders shipped to pennsylvania 
+SELECT *
+FROM orders o 
+WHERE ship_state = 'Pennsylvania' ;
+
+
+-- List the first name and last name of all employees with last names that start with the letter W
+SELECT first_name, last_name
+FROM  employees e 
+WHERE last_name 
+LIKE 'W%' ;
+
+-- List all customers from zip codes that start with 55
+SELECT *
+FROM customers c 
+WHERE postal_code 
+LIKE '55%' ;
+
+-- List all the customers from zip codes that end with 0
+SELECT *
+FROM customers c 
+WHERE postal_code 
+LIKE '%0' ;
+
+-- List the first name, last name, and email for all customers with a .org email address
+SELECT first_name, last_name, email  
+FROM customers c
+WHERE email 
+LIKE '%.edu' ;
+
+-- List the first name, last name, and email for all customers from the 202 area code
+SELECT first_name, last_name, email  
+FROM customers c 
+WHERE phone  
+LIKE '1-(202%' ;
+
+-- List the first name, last name, and phone number for all customers from the 202 area code, ordered 
+-- by last name, first name
+SELECT last_name, first_name, phone  
+FROM customers c 
+WHERE phone 
+LIKE '1-(202%'
+ORDER BY last_name asc, first_name asc ;
